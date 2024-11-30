39b794d8991245b4f9b993df2eb8db32be421bad
Pavle <topalovic03@gmail.com>
2024-11-30 17:40:35 +0100

    login register front

---
+++ frontend-trello/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';  
+import { AppRoutingModule } from './app-routing.module';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt'; 
+
+export function tokenGetter() {
+  return localStorage.getItem('jwtToken');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,  
+    ReactiveFormsModule,  
+    JwtModule.forRoot({ 
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:8000'],  
+        disallowedRoutes: []  
+      }
+    })
+  ],
+  providers: [JwtHelperService],  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
